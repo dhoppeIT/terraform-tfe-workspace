344b472d35add653ddad436699cef814761bbb1b
Dennis Hoppe <github@dhoppe.it>
2022-02-02 17:27:52 +0100

    chore: Add code examples (#20)
    * chore: Add code examples
    
    * docs: Generate README.md
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ examples/workspace_without_vcs/locals.tf
@@ -0,0 +1,6 @@
+locals {
+  organization_name  = "dhoppeIT"
+  organization_email = "terraform@dhoppe.it"
+
+  workspace_name = "terraform"
+}
---
+++ examples/workspace_without_vcs/variables.tf
@@ -0,0 +1,5 @@
+variable "token" {
+  type        = string
+  default     = null
+  description = "The token used to authenticate with Terraform Cloud/Enterprise"
+}
